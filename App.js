4747980d03196cafcc743e2e3d226c9fa6d55c9b
tfitzpatrick0 <tim.fitz830@gmail.com>
2022-06-26 15:06:23 -0700

    change header

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ const styles = StyleSheet.create({
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>My gains tracker.</Text>
       <StatusBar style="auto" />
     </View>
   );
